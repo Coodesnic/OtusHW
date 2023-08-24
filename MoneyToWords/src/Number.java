542ecb5320bb789bd8beb08d71ad1b16606df11b
Coodesnic <132272639+Coodesnic@users.noreply.github.com>
2023-08-24 18:14:27 +0300

    Add files via upload

---
+++ MoneyToWords/src/Number.java
@@ -0,0 +1,9 @@
+public class Number {
+    int value;
+    String russian;
+    //String english;
+    public Number(int value,String russian){
+        this.value = value;
+        this.russian = russian;
+    }
+}
