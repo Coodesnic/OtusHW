dd48d80b707cb5ee33292105e67c1fed1c97b3e2
Coodesnic <132272639+Coodesnic@users.noreply.github.com>
2023-08-25 16:00:07 +0300

    Delete MoneyToWords/src directory

--- MoneyToWords/src/Number.java
+++
@@ -1,9 +0,0 @@
-public class Number {
-    int value;
-    String russian;
-    //String english;
-    public Number(int value,String russian){
-        this.value = value;
-        this.russian = russian;
-    }
-}
